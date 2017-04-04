eb6a3bac29855ef045805fa974f4069b21ba9a89
Justin Toh <tohjustin@hotmail.com>
2017-04-03 23:00:04 -0700

    Corrected touch area for buttons

--- views/_main.c
+++ views/_main.c
@@ -2,30 +2,42 @@
 #include "../modules/images/images.h"
 #include "../modules/touchscreen/touchscreen.h"
 
+#define CHECKIN_BUTTON_POSITION_X 16 + VIEW_POSITION_X
+#define CHECKIN_BUTTON_POSITION_Y 16 + VIEW_POSITION_Y
+#define CHECKIN_BUTTON_HEIGHT 128
+#define CHECKIN_BUTTON_WIDTH 128
+
+#define LOGOUT_BUTTON_POSITION_X 16 + VIEW_POSITION_X
+#define LOGOUT_BUTTON_POSITION_Y 150 + VIEW_POSITION_Y
+#define LOGOUT_BUTTON_HEIGHT 32
+#define LOGOUT_BUTTON_WIDTH 128
+
 int 
 loadView_main(){
 
     Point touchInput;
-    Box button_continue;
     int next_state;
+
+    /* Setup touch area for [CHECKIN_BUTTON] & [LOGOUT_BUTTON] */
+    Box button_checkin = createBox(CHECKIN_BUTTON_POSITION_X, CHECKIN_BUTTON_POSITION_Y, CHECKIN_BUTTON_HEIGHT, CHECKIN_BUTTON_WIDTH);
+    Box button_logout = createBox(LOGOUT_BUTTON_POSITION_X, LOGOUT_BUTTON_POSITION_Y, LOGOUT_BUTTON_HEIGHT, LOGOUT_BUTTON_WIDTH);
 
     /* Draw UI on screen */
     drawView_main();
 
-    /* Setup touch area for [CONTINUE_BUTTON] */
-    button_continue.x = VIEW_POSITION_X;
-    button_continue.y = VIEW_POSITION_Y;
-    button_continue.width = VIEW_WIDTH;
-    button_continue.height = VIEW_HEIGHT;
-
-    /* Listen for inputs on the touchscreen */
     while(1){
+        /* Listen for inputs on the touchscreen */
         if ((TOUCHSCREEN_STATUS & TOUCHSCREEN_STATUS_RX_MASK) == TOUCHSCREEN_STATUS_RX_MASK) {
             touchInput = TouchPressed();
             touchInput = TouchRelease();
 
-            /* Check if touch input fall within the [CONTINUE_BUTTON] */
-            if (isTouchInputWithinBox(touchInput, button_continue)) {
+            /* Check if touch input fall within the [LOGOUT_BUTTON] */
+            if (isTouchInputWithinBox(touchInput, button_checkin)) {
+                printf("CHECKING IN...\n");
+            }
+
+            /* Check if touch input fall within the [LOGOUT_BUTTON] */
+            if (isTouchInputWithinBox(touchInput, button_logout)) {
                 next_state = VIEW_SPLASHSCREEN_ID;
                 break;
             }
--- views/_splashscreen.c
+++ views/_splashscreen.c
@@ -2,21 +2,22 @@
 #include "../modules/images/images.h"
 #include "../modules/touchscreen/touchscreen.h"
 
+#define CONTINUE_BUTTON_POSITION_X VIEW_POSITION_X
+#define CONTINUE_BUTTON_POSITION_Y VIEW_POSITION_Y
+#define CONTINUE_BUTTON_HEIGHT VIEW_WIDTH
+#define CONTINUE_BUTTON_WIDTH VIEW_HEIGHT
+
 int
 loadView_splashscreen(){
 
     Point touchInput;
-    Box button_continue;
     int next_state;
+
+    /* Setup touch area for [CONTINUE_BUTTON] */
+    Box button_continue = createBox(CONTINUE_BUTTON_POSITION_X, CONTINUE_BUTTON_POSITION_Y, CONTINUE_BUTTON_HEIGHT, CONTINUE_BUTTON_WIDTH);
 
     /* Draw UI on screen */
     drawView_splashscreen();
-
-    /* Setup touch area for [CONTINUE_BUTTON] */
-    button_continue.x = VIEW_POSITION_X;
-    button_continue.y = VIEW_POSITION_Y;
-    button_continue.width = VIEW_WIDTH;
-    button_continue.height = VIEW_HEIGHT;
 
     /* Listen for inputs on the touchscreen */
     while(1){
