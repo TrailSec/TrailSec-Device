b771221e5f2d581637dcd1d259181970364ec203
Justin Toh <tohjustin@hotmail.com>
2017-04-05 11:00:30 -0700

    Change splashscreen to switch to bluetooth pairing view  after a specificied time delay

--- views/view_splashscreen.c
+++ views/view_splashscreen.c
@@ -1,37 +1,21 @@
+#include <unistd.h>
 #include "views.h"
 #include "../modules/images/images.h"
 #include "../modules/touchscreen/touchscreen.h"
 
-#define CONTINUE_BUTTON_POSITION_X VIEW_POSITION_X
-#define CONTINUE_BUTTON_POSITION_Y VIEW_POSITION_Y
-#define CONTINUE_BUTTON_HEIGHT VIEW_WIDTH
-#define CONTINUE_BUTTON_WIDTH VIEW_HEIGHT
+#define SPLASHSCREEN_DELAY 2 * 1000 * 1000 // 4 second
 
 int
 loadView_splashscreen(){
 
     Point touchInput;
-    int next_state;
-
-    /* Setup touch area for [CONTINUE_BUTTON] */
-    Box button_continue = createBox(CONTINUE_BUTTON_POSITION_X, CONTINUE_BUTTON_POSITION_Y, CONTINUE_BUTTON_HEIGHT, CONTINUE_BUTTON_WIDTH);
+    int next_state = VIEW_BLUETOOTHPAIRING_ID;
 
     /* Draw UI on screen */
     drawView_splashscreen();
 
-    /* Listen for inputs on the touchscreen */
-    while(1){
-        if ((TOUCHSCREEN_STATUS & TOUCHSCREEN_STATUS_RX_MASK) == TOUCHSCREEN_STATUS_RX_MASK) {
-            touchInput = TouchPressed();
-            touchInput = TouchRelease();
-
-            /* Check if touch input fall within the [CONTINUE_BUTTON] */
-            if (isTouchInputWithinBox(touchInput, button_continue)) {
-                next_state = VIEW_BLUETOOTHPAIRING_ID;
-                break;
-            }
-        }
-    }
+    /* Wait for 1s before switching to the new view */
+    usleep(SPLASHSCREEN_DELAY);
 
     return next_state;
 };
